6e5b5798ca445d048da12ce7b6dde9cc3d994e04
Exist Model <sarizava@yandex.ru>
2018-01-05 14:46:53 +0300

    Removed the piece of code , fixed links

--- js/script.js
+++ js/script.js
@@ -20,11 +20,12 @@
 
       $(document).ready(function() {
         $(".portfolio__popup").magnificPopup({type:"image"});
-        function slowScroll(id) {
-          var offset = 0;
-          $("html, body").animate ({
-            scrollTop: $(id).offset ().top - offset
-          }, 1000);
-          return false;
-        }
       });
+
+function slowScroll(id) {
+      var offset = 0;
+      $("html, body").animate ({
+        scrollTop: $(id).offset ().top - offset
+      }, 1000);
+      return false;
+}
