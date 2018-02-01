0f009a18da88f71de7d71e6e3467330577b447b2
Exist Model <sarizava@yandex.ru>
2018-02-01 14:25:58 +0300

    Test service workers

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/js/sw.js').then(function(reg) {
+  navigator.serviceWorker.register('/js/sw.js', { scope: '/js/' }).then(function(reg) {
     // регистрация сработала
     console.log('Registration succeeded. Scope is ' + reg.scope);
   }).catch(function(error) {
