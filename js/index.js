aa764f015e322aed01ed02df40ea09e43de1c8eb
Exist Model <sarizava@yandex.ru>
2018-02-01 14:18:08 +0300

    Test service workers

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 if ('serviceWorker' in navigator) {
-    // Весь код регистрации у нас асинхронный.
-    navigator.serviceWorker.register('sw.js')
-      .then(() => navigator.serviceWorker.ready.then((worker) => {
-        worker.sync.register('syncdata');
-      }))
-      .catch((err) => console.log(err));
-}
+  navigator.serviceWorker.register('/js/sw.js').then(function(reg) {
+    // регистрация сработала
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch(function(error) {
+    // регистрация прошла неудачно
+    console.log('Registration failed with ' + error);
+  });
+};
--- js/sw.js
+++ js/sw.js
@@ -1,36 +1,11 @@
-// self.addEventListener('install', (event) => {
-//     console.log('Установлен');
-// });
-
-// self.addEventListener('activate', (event) => {
-//     console.log('Активирован');
-// });
-
-// self.addEventListener('fetch', (event) => {
-//     console.log('Происходит запрос на сервер');
-// });
-
-const CACHE = 'cache-only-v1';
-
-// При установке воркера мы должны закешировать часть данных (статику).
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE).then((cache) => {
-            return cache.addAll([
-                '../img'
-            ]);
-        })
-    );
+    console.log('Установлен');
 });
 
-// При запросе на сервер (событие fetch), используем только данные из кэша.
-self.addEventListener('fetch', (event) =>
-    event.respondWith(fromCache(event.request));
-);
+self.addEventListener('activate', (event) => {
+    console.log('Активирован');
+});
 
-function fromCache(request) {
-    return caches.open(CACHE).then((cache) =>
-      cache.match(request)
-          .then((matching) => matching || Promise.reject('no-match'))
-    );
-}
+self.addEventListener('fetch', (event) => {
+    console.log('Происходит запрос на сервер');
+});
